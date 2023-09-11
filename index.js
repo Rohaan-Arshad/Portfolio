aec068bc12cfc36f05378cb80e661b6c0ea52f39
Rohaan-Arshad <rohaanjutt112@gmail.com>
2023-09-12 01:09:26 +0500

    About Section

--- index.js
+++ index.js
@@ -25,4 +25,17 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".home").classList.add("new_image");
     }, 2000); 
 });
+
+/* add in every   psss about exceptof setion anem*/
+document.addEventListener('DOMContentLoaded', function() {
+  const aboutSection = document.querySelector('.about');
+  const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+          aboutSection.classList.add('loaded');
+          observer.disconnect();
+      }
+  });
+  observer.observe(aboutSection);
+});
+
   
